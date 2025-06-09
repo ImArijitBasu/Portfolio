7611f8165e80eff9430cddfa3cc43342c23bdd0e
Arijit <arijitbasubd@gmail.com>
2025-06-10 00:22:42 +0600

    navbar update

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   const navButtons = (
     <>
       <li>
-        <Link to={'/'}><a href="#home" className="text-black hover:text-blue-500">
+        <a
+          href="#home"
+          className={` text-xl font-bold  ${
+            isScrolled
+              ? " text-black hover:text-blue-500"
+              : " text-white hover:text-red-500"
+          } transition-all duration-300 px-5 mx-1`}
+        >
           Home
-        </a></Link>
+        </a>
       </li>
       <li>
-        <a href="#about" className="text-black hover:text-blue-500">
+        <a
+          href="#about"
+          className={` text-xl font-bold  ${
+            isScrolled
+              ? " text-black hover:text-blue-500"
+              : " text-white hover:text-red-500"
+          } transition-all duration-300 px-5 mx-1`}
+        >
           About
         </a>
       </li>
       <li>
-        <a href="#skills" className="text-black hover:text-blue-500">
+        <a
+          href="#skills"
+          className={` text-xl font-bold  ${
+            isScrolled
+              ? " text-black hover:text-blue-500"
+              : " text-white hover:text-red-500"
+          } transition-all duration-300 px-5 mx-1`}
+        >
           Skills
         </a>
       </li>
       <li>
-        <a href="#projects" className="text-black hover:text-blue-500">
+        <a
+          href="#projects"
+          className={` text-xl font-bold  ${
+            isScrolled
+              ? " text-black hover:text-blue-500"
+              : " text-white hover:text-red-500"
+          } transition-all duration-300 px-5 mx-1`}
+        >
           Projects
         </a>
       </li>
@@ -29,51 +67,64 @@ const Navbar = () => {
 
   return (
     <div>
-     <div className="navbar bg-blue-100 bg-opacity-50 backdrop-blur-lg border-b-2 border-r-8 border-blue-700 fixed top-0 container mx-auto z-10">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16"
-          />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+      <div
+        className={`navbar backdrop-blur-lg border-b-2 border-r-8 border-r-transparent border-b-blue-700 sm:border-transparent md:border-transparent lg:border-blue-700 md:rounded-r-full lg:rounded-r-full fixed top-0 left-0 container z-10 ${
+          isScrolled
+            ? "bg-blue-100 bg-opacity-50 mt-0"
+            : "bg-blue-700 text-white mt-1"
+        } transition-all duration-300`}
       >
-        {navButtons}
-      </ul>
-    </div>
-
-    <img width="100px" className="object-fill" src="/logo.png" alt="" />
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">{navButtons}</ul>
-  </div>
-  <div className="navbar-end">
-    <a
-      href="https://drive.google.com/file/d/1PeQ4zS_2dHBO2VY9LbirPprkwmO1rMk9/view?usp=drive_link"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <button className="rounded-2xl border-2 border-dashed border-black bg-transparent px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
-        Resume
-      </button>
-    </a>
-  </div>
-</div>
-
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navButtons}
+            </ul>
+          </div>
+          {/* <img width="100px" className="object-fill" src="/logo.png" alt="" /> */}
+          <h1 className="uppercase font-extrabold text-4xl">
+            ar<span className="text-red-500">i</span>jit
+          </h1>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navButtons}</ul>
+        </div>
+        <div className="navbar-end">
+          <a
+            href="https://drive.google.com/file/d/1PeQ4zS_2dHBO2VY9LbirPprkwmO1rMk9/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className={`rounded-2xl border-2 border-dashed border-black px-6 py-3 font-semibold uppercase transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none ${
+                isScrolled
+                  ? "text-black bg-transparent"
+                  : "text-blue-700 bg-blue-100"
+              }`}
+            >
+              Resume
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
